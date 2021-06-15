ed4d99f287dcbfe416cfafd3878725dc90ccd2cc
supminn <30731236+supminn@users.noreply.github.com>
2021-06-15 15:28:56 +0530

    fix: redirects

--- src/App.js
+++ src/App.js
@@ -42,27 +42,23 @@ function App() {
   return (
     <div className="bg-blue-50 text-blue-900 min-h-screen relative">
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-      {user.status === "fulfilled" && posts.status === "fulfilled" && (
-        <div className="px-2 min-h-body">
-          <Routes>
-            <PrivateRoute path="/home" element={<Home />} />
-            <PrivateRoute path="/:username" element={<UserProfile />} />
-            <PrivateRoute path="/:username/profile" element={<EditProfile />} />
-            <PrivateRoute path="/:username/following" element={<Following />} />
-            <PrivateRoute path="/:username/followers" element={<Followers />} />
-            <PrivateRoute
-              path="/:username/post/:postId"
-              element={<PostDetails />}
-            />
-            <PrivateRoute path="/search" element={<SearchUsers />} />
-          </Routes>
-        </div>
-      )}
+      <div className="px-2 min-h-body">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <PrivateRoute path="/home" element={<Home />} />
+          <PrivateRoute path="/:username" element={<UserProfile />} />
+          <PrivateRoute path="/:username/profile" element={<EditProfile />} />
+          <PrivateRoute path="/:username/following" element={<Following />} />
+          <PrivateRoute path="/:username/followers" element={<Followers />} />
+          <PrivateRoute
+            path="/:username/post/:postId"
+            element={<PostDetails />}
+          />
+          <PrivateRoute path="/search" element={<SearchUsers />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
